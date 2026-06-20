8e34cdf054e8fcd8b153b2e09cd15dfbfb0d287a
Boymahmad <boymahmadhusenov@gmail.com>
2026-06-20 05:37:36 -1200

    Fix: add WhiteNoise for static files

--- smart_quiz/settings.py
+++ smart_quiz/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -93,3 +94,13 @@ LOGOUT_REDIRECT_URL = 'quiz:login'
 
 # 🔑 OPENAI
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
